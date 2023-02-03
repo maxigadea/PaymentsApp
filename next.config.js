44c66c3a54ed6314362cd9f266a51075970f9164
maxi.gadea <maxi.gadea@bitnovo.com>
2023-02-03 13:35:07 -0300

    make pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
+
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+
+const PWAConfig = require("next-pwa");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
-module.exports = nextConfig
+const withPWA = PWAConfig({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+module.exports = withPWA(nextConfig);
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 import Payment from '../components/payment/payment'
 import styles from '../styles/Home.module.css'
 
